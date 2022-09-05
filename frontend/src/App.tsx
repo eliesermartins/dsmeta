2af7e3ed221527fc4bdcc4ac6208645964b83d50
eliesermartins <ejmartins81@gmail.com>
2022-09-05 17:37:33 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,19 @@
-import Header from "./components/Header"
-import SalesCard from "./components/SalesCard"
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
-  return(
+  return (
     <>
       <Header />
       <main>
         <section id="sales">
           <div className="dsmeta-container">
-            <SalesCard />            
+            <SalesCard />
           </div>
         </section>
-      </main>  
+      </main>
     </>
-  )          
+  )
 }
 
-export default App
+export default App;
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,19 +1,19 @@
-import logo from '../../assets/img/logo.svg'
-import './styles.css'
+import logo from '../../assets/img/logo.svg';
+import './styles.css';
 
 function Header() {
-  return(
+  return (
     <header>
-        <div className="dsmeta-logo-container">
-            <img src={logo} alt="DSMeta" />
-            <h1>DSMeta</h1>
-            <p>
-              Desenvolvido por
-              <a href="https://www.instagram.com/elieser.martins.7/">@eliesermartins</a>
-            </p>
-        </div>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://www.instagram.com/elieser.martins.7/">@eliesermartins</a>
+        </p>
+      </div>
     </header>
-  )          
+  )
 }
 
-export default Header
+export default Header;
